0f019b9d09a5ffe10f9e1711f465c6704449e1aa
Kevw94 <pinerokevin@icloud.com>
2024-10-10 15:48:32 +0200

    Add working Time

--- time_manager/api/lib/time_manager/times/working_time.ex
+++ time_manager/api/lib/time_manager/times/working_time.ex
@@ -3,8 +3,8 @@ defmodule TimeManager.Times.WorkingTime do
   import Ecto.Changeset
 
   schema "workingtimes" do
-    field :end, :naive_datetime
-    field :start, :naive_datetime
+    field :end, :utc_datetime
+    field :start, :utc_datetime
     field :user_id, :id
 
     timestamps(type: :utc_datetime)
@@ -13,7 +13,7 @@ defmodule TimeManager.Times.WorkingTime do
   @doc false
   def changeset(working_time, attrs) do
     working_time
-    |> cast(attrs, [:start, :end])
-    |> validate_required([:start, :end])
+    |> cast(attrs, [:start, :end, :user_id])
+    |> validate_required([:start, :end, :user_id])
   end
 end
--- time_manager/api/lib/time_manager_web/controllers/working_time_controller.ex
+++ time_manager/api/lib/time_manager_web/controllers/working_time_controller.ex
@@ -12,7 +12,8 @@ defmodule TimeManagerWeb.WorkingTimeController do
   end
 
   def create(conn, %{"working_time" => working_time_params}) do
-    with {:ok, %WorkingTime{} = working_time} <- Times.create_working_time(working_time_params) do
+    user_id = working_time_params["user_id"]
+    with {:ok, %WorkingTime{} = working_time} <- Times.create_working_time(user_id, working_time_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", ~p"/api/workingtime/#{working_time.user_id}/#{working_time.id}")
