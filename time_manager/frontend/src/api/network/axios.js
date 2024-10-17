bf6eaa01c5e55791aa003aadbf741c8ec70a0112
Kevw94 <pinerokevin@icloud.com>
2024-10-17 11:49:30 +0200

    Change url call api

--- time_manager/frontend/src/api/network/axios.js
+++ time_manager/frontend/src/api/network/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const http = axios.create({
-	baseURL: "http://localhost:4000/api/",
+	baseURL: "http://127.0.0.1:4000/api/",
 });
 
 http.defaults.headers.post['Content-Type'] = 'application/json';
