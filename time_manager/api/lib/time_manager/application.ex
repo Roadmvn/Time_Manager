a692b717e0189233b3e15097f6af1c2c9ebfada6
Kevw94 <pinerokevin@icloud.com>
2024-10-18 16:25:19 +0200

    Implement .env module for back

--- time_manager/api/lib/time_manager/application.ex
+++ time_manager/api/lib/time_manager/application.ex
@@ -7,6 +7,10 @@ defmodule TimeManager.Application do
 
   @impl true
   def start(_type, _args) do
+    unless Mix.env == :prod do
+      Dotenv.load
+      Mix.Task.run("loadconfig")
+    end
     children = [
       TimeManagerWeb.Telemetry,
       TimeManager.Repo,
--- time_manager/api/mix.exs
+++ time_manager/api/mix.exs
@@ -60,6 +60,7 @@ defmodule TimeManager.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:cors_plug, "~> 3.0"},
+      {:dotenv, "~> 3.0.0"}
     ]
   end
 
