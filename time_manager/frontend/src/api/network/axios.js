74d71f103816ee88fdb40834ccb783ff01031d71
Roadmvn <tudy.gbaguidi@epitech.eu>
2024-10-21 11:19:50 +0200

    fix

--- time_manager/frontend/src/api/network/axios.js
+++ time_manager/frontend/src/api/network/axios.js
@@ -1,25 +1,7 @@
 import axios from 'axios';
-import store from '../../store';
 
-const instance = axios.create({
-	baseURL: 'http://localhost:4000/api'
+
+export const http = axios.create({
+	baseURL: 'http://localhost:4000/api',
+	withCredentials: true
 });
-
-instance.interceptors.request.use(
-	(config) => {
-		const token = store.getters['auth/token'];
-		if (token) {
-			config.headers['Authorization'] = `Bearer ${token}`;
-		}
-		const xsrfToken = document.cookie.split('; ').find(row => row.startsWith('xsrf-token='));
-		if (xsrfToken) {
-			config.headers['X-XSRF-TOKEN'] = xsrfToken.split('=')[1];
-		}
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
-);
-
-export default instance;
